ab81c6ee2b6e89d610e036aa82670a35da14b323
ademi <ademirel1971@yahoo.com>
2021-08-25 11:35:05 +0200

    Ilk ayarlar yapildi

---
+++ src/test/java/ZippoTest.java
@@ -0,0 +1,19 @@
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+
+public class ZippoTest {
+
+    @Test
+    public void Test() {
+        given()
+                // hazirlik islemlerini yapacagiz
+                .when()
+                // link ve aksiyon islemleri
+                .then()
+        // test extract islemleri
+        ;
+    }
+}
